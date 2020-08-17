bad6cb4fc71bcc7de362e72dbe5f8349d08a2826
ilyas <gamidov@tut.by>
2020-08-17 23:36:05 +0300

    else three mini-sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,37 @@
 import MainSlider from './modules/slider/slider-main';
 import VideoPlayer from './modules/videoPlayer';
+import MiniSlider from './modules/slider/slider-mini';
+
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new MainSlider({page: '.page', btns: '.next'});
+    const slider = new MainSlider({container: '.page', btns: '.next'});
     slider.render();
+
+    const showUpSlider = new MiniSlider({
+        container: '.showup__content-slider',
+        next: '.showup__next',
+        prev: '.showup__prev',
+        activeClass: 'card-active',
+        animate: true
+    });
+    showUpSlider.init();
+
+    const modulesSlider = new MiniSlider({
+        container: '.modules__content-slider',
+        next: '.slick-next',
+        prev: '.slick-prev',
+        activeClass: 'card-active',
+        animate: true
+    });
+    modulesSlider.init();
+
+    const feedSlider = new MiniSlider({
+        container: '.feed__slider',
+        next: '.feed__slider .slick-next',
+        prev: '.feed__slider .slick-prev',
+        activeClass: 'feed__item-active',
+    });
+    feedSlider.init();
 
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
