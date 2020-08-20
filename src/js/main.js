4deb635b92696cf7755c85ff4b006ff360a15e3b
ilyas <gamidov@tut.by>
2020-08-20 15:37:39 +0300

    download file

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import MiniSlider from './modules/slider/slider-mini';
 import Difference from './modules/difference';
 import Form from './modules/form';
 import ShowInfo from './modules/showInfo';
+import Download from './modules/download';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({container: '.page', btns: '.next'});
@@ -44,4 +45,5 @@ window.addEventListener('DOMContentLoaded', () => {
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
     new Form('.form').init();
     new ShowInfo('.plus__content').init();
+    new Download('.download').init();
 });
