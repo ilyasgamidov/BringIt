fad0844b38224014ae55ce05e50c09f61d2a6987
ilyas <gamidov@tut.by>
2020-08-20 15:04:17 +0300

    show info

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import VideoPlayer from './modules/videoPlayer';
 import MiniSlider from './modules/slider/slider-mini';
 import Difference from './modules/difference';
 import Form from './modules/form';
+import ShowInfo from './modules/showInfo';
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new MainSlider({container: '.page', btns: '.next'});
@@ -42,4 +43,5 @@ window.addEventListener('DOMContentLoaded', () => {
 
     new Difference('.officerold', '.officernew', '.officer__card-item').init();
     new Form('.form').init();
+    new ShowInfo('.plus__content').init();
 });
